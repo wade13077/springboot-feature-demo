97574d1da5165a74a2d6aaa1d299e3cb6c2fa667
wade077 <wade841208@gmail.com>
2025-08-15 23:46:09 +0800

    feat: init new project

---
+++ springboot-feature-demo/src/main/java/com/wade/springboot/springboot_feature_demo/SpringbootFeatureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wade.springboot.springboot_feature_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFeatureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFeatureDemoApplication.class, args);
+	}
+
+}
